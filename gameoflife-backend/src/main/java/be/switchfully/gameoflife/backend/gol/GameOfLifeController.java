709060a56aa4677cdb5a59dc1ec0137183d6e11f
Gerri Vanderhaegen <gerri.vanderhaegen@cegeka.com>
2018-07-16 14:49:22 +0200

    commit for new pc

--- gameoflife-backend/src/main/java/be/switchfully/gameoflife/backend/gol/GameOfLifeController.java
+++ gameoflife-backend/src/main/java/be/switchfully/gameoflife/backend/gol/GameOfLifeController.java
@@ -17,10 +17,85 @@ public class GameOfLifeController {
     private static Logger logger = Logger.getLogger(GameOfLifeController.class);
 
     @PostMapping(value = "/phase")
-    public List<List<Boolean>> nextGeneration(@RequestBody List<List<Boolean>> currentWorld){
+    public List<List<Boolean>> nextGeneration(@RequestBody List<List<Boolean>> currentWorld) {
         logger.info(currentWorld);
-        Collections.shuffle(currentWorld);
+//        Collections.shuffle(currentWorld);
         return currentWorld;
     }
 
+    public static int counter(List<List<Boolean>> world, int row, int col) {
+        int counter = checkNormalCases(world, row, col)+checkUpperCorners(world, row, col)+checkLowerCorners(world, row, col);
+        if (world.get(row).get(col)) {
+            counter = counter - 1;
+        }
+        return counter;
+    }
+
+    private static int checkLowerCorners(List<List<Boolean>> world, int row, int col) {
+    int counter = 0;
+        if (row == world.size() - 1) {
+            if (col == 0) {
+                for (int rij = row - 1; rij < row + 1; rij++) {
+                    for (int kol = col; kol < col + 2; kol++) {
+                        if (world.get(rij).get(kol)) {
+                            counter++;
+                        }
+                    }
+                }
+            }
+            if (col == world.size() - 1) {
+                for (int rij = row - 1; rij < row + 1; rij++) {
+                    for (int kol = col - 1; kol < col + 1; kol++) {
+                        if (world.get(rij).get(kol)) {
+                            counter++;
+                        }
+
+                    }
+                }
+            }
+        }
+        return counter;
+    }
+
+    private static int checkUpperCorners(List<List<Boolean>> world, int row, int col) {
+        int counter = 0;
+        if (row == 0) {
+            if (col == world.size() - 1) {
+                for (int rij = row; rij < row + 2; rij++) {
+                    for (int kol = col - 1; kol < col + 1; kol++) {
+                        if (world.get(rij).get(kol)) {
+                            counter++;
+                        }
+
+                    }
+                }
+            }
+            if (col == 0) {
+                for (int rij = row; rij < row + 2; rij++) {
+                    for (int kol = col; kol < col + 2; kol++) {
+                        if (world.get(rij).get(kol)) {
+                            counter++;
+                        }
+
+                    }
+                }
+            }
+        }
+        return counter;
+    }
+
+    private static int checkNormalCases(List<List<Boolean>> world, int row, int col) {
+        int counter = 0;
+        if (row != 0 && row != world.size() - 1 && col != 0 && col != world.size() - 1) {
+            for (int rij = row - 1; rij < row + 2; rij++) {
+                for (int kol = col - 1; kol < col + 2; kol++) {
+                    if (world.get(rij).get(kol)) {
+                        counter++;
+                    }
+
+                }
+            }
+        }
+        return counter;
+    }
 }
--- gameoflife-backend/src/test/java/be/switchfully/gameoflife/backend/gol/GameOfLifeControllerTest.java
+++ gameoflife-backend/src/test/java/be/switchfully/gameoflife/backend/gol/GameOfLifeControllerTest.java
@@ -21,10 +21,6 @@ public class GameOfLifeControllerTest extends UnitTest {
         List<Boolean> rij2 = new ArrayList<>();
         List<Boolean> rij3 = new ArrayList<>();
 
-        testGrid.add(rij1);
-        testGrid.add(rij2);
-        testGrid.add(rij3);
-
         rij1.add(false);
         rij1.add(true);
         rij1.add(true);
@@ -34,6 +30,11 @@ public class GameOfLifeControllerTest extends UnitTest {
         rij3.add(true);
         rij3.add(false);
         rij3.add(true);
+
+        testGrid.add(rij1);
+        testGrid.add(rij2);
+        testGrid.add(rij3);
+
 
     }
 
@@ -54,9 +55,47 @@ public class GameOfLifeControllerTest extends UnitTest {
     }
 
     @Test
-    public void givenCoordinates1_1_ShouldReturnCorrectNumberOfNeighbours() throws Exception {
-        
+    public void givenCoordinates1_1_ShouldReturnInCorrectNumberOfNeighbours() throws Exception {
+        int row = 1;
+        int col = 1;
+        int actual = GameOfLifeController.counter(testGrid, row, col);
 
-        Assertions.assertThat(1).isEqualTo(1);
+        Assertions.assertThat(actual).isEqualTo(5);
+    }
+
+    @Test
+    public void givenCoordinates0_0_shouldReturnCorrectCounter() throws Exception {
+        int row = 0;
+        int col = 0;
+        int actual = GameOfLifeController.counter(testGrid, row, col);
+
+        Assertions.assertThat(actual).isEqualTo(2);
+
+    }
+    @Test
+    public void givenCoordinates2_2_shouldReturnCorrectCounter() throws Exception {
+        int row = 2;
+        int col = 2;
+        int actual = GameOfLifeController.counter(testGrid, row, col);
+
+        Assertions.assertThat(actual).isEqualTo(2);
+
+    }
+
+    @Test
+    public void givenCoordinates0_2_shouldReturnCorrectCounter() throws Exception {
+        int row = 0;
+        int col = 2;
+        int actual = GameOfLifeController.counter(testGrid,row,col);
+
+        Assertions.assertThat(actual).isEqualTo(3);
+    }
+    @Test
+    public void givenCoordinates2_0_shouldReturnCorrectCounter() throws Exception {
+        int row = 2;
+        int col = 0;
+        int actual = GameOfLifeController.counter(testGrid,row,col);
+
+        Assertions.assertThat(actual).isEqualTo(1);
     }
 }
